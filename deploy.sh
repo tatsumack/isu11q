4f8b93587fb1647cbe0bdf7453c5f66e04f6eed5
tatsumack <tatsu.mack@gmail.com>
2021-08-21 11:30:50 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,124 @@
+#!/bin/sh
+set -ex 
+
+echo "Deploy script started."
+
+PROJECT_ROOT=/home/isucon/webapp
+
+LOG_BACKUP_DIR=/var/log/isucon
+
+USER=isucon
+KEY_OPTION="-A"
+
+WEB_SERVERS="isu01"
+APP_SERVERS="isu01"
+DB_SERVER="isu01"
+
+BACKUP_TARGET_LIST="/var/log/nginx/access.log /var/log/nginx/error.log"
+
+BRANCH=$1
+if [ -z "$BRANCH" ]; then
+  BRANCH="master"
+fi
+
+# sed -n -r 's/^(LogFormat.*)(" combined)/\1 %D\2/p' /etc/httpd/conf/httpd.conf
+echo "Stop Web Server"
+for WEB_SERVER in $WEB_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$WEB_SERVER sh
+sudo systemctl stop nginx
+EOS
+done
+
+echo "Stop Application Server"
+for APP_SERVER in $APP_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+sudo systemctl stop isucondition.go.service
+EOS
+done
+
+echo "Stop DataBase Server"
+cat <<EOS | ssh $KEY_OPTION $USER@$DB_SERVER sh
+sudo systemctl stop mysql
+EOS
+
+echo "Get Current git hash"
+for APP_SERVER in $APP_SERVERS
+do
+hash=`cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+cd $PROJECT_ROOT
+git rev-parse --short HEAD
+EOS`
+echo "Current Hash: $hash"
+done
+
+set +e
+LOG_DATE=`date +"%H%M%S"`
+echo "Backup App Server LOG"
+for LOG_PATH in $BACKUP_TARGET_LIST
+do
+    LOG_FILE=`basename $LOG_PATH`
+for APP_SERVER in $APP_SERVERS
+do
+    cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+sudo mkdir -p ${LOG_BACKUP_DIR}
+sudo mv $LOG_PATH ${LOG_BACKUP_DIR}/${LOG_FILE}_${LOG_DATE}_${hash}
+EOS
+done
+done
+for DB_SERVER in $DB_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$DB_SERVER sh
+sudo rm /var/log/mysql/mysqld-slow.log
+EOS
+done
+set -e
+
+echo "Current Hash: $hash"
+echo "Update Project"
+for APP_SERVER in $APP_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+cd $PROJECT_ROOT
+git clean -fd
+git reset --hard
+git fetch -p
+git checkout $BRANCH
+git pull --rebase
+cd go
+PATH=/home/isucon/local/go/bin:/home/isucon/go/bin:/usr/bin go build -o isucondition main.go
+EOS
+done
+echo "Get new git hash"
+for APP_SERVER in $APP_SERVERS
+do
+new_hash=`cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+cd $PROJECT_ROOT
+git rev-parse --short HEAD
+EOS`
+echo "Current Hash: $new_hash"
+done
+echo "Start Database Server"
+cat <<EOS | ssh $KEY_OPTION $USER@$DB_SERVER sh
+sudo swapoff -a && sudo swapon -a
+sudo systemctl start mysql
+EOS
+echo "Start App Server"
+for APP_SERVER in $APP_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$APP_SERVER sh
+sudo swapoff -a && sudo swapon -a
+sudo systemctl start isucondition.go.service
+EOS
+done
+echo "Start Web Server"
+for WEB_SERVER in $WEB_SERVERS
+do
+cat <<EOS | ssh $KEY_OPTION $USER@$WEB_SERVER sh
+sudo swapoff -a && sudo swapon -a
+sudo systemctl start nginx
+EOS
+done
+echo "Deploy script finished."
+
