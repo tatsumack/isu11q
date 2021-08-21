633ca91cdf7af3ac4c0ace10a3451e86f18897a9
tatsumack <tatsu.mack@gmail.com>
2021-08-21 16:52:35 +0900

    condition limit 100

--- go/main.go
+++ go/main.go
@@ -30,7 +30,7 @@ import (
 
 const (
 	sessionName                 = "isucondition_go"
-	conditionLimit              = 20
+	conditionLimit              = 100
 	frontendContentsPath        = "../public"
 	jiaJWTSigningKeyPath        = "../ec256-public.pem"
 	defaultIconDirPath          = "../icons/"
