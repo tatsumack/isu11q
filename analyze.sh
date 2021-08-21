db1c609d0908fab04d3900348d20653bd1d11a87
naoya.tabakomori <cigaret.cigaret@gmail.com>
2021-08-21 12:05:23 +0900

    aggregateするように

--- analyze.sh
+++ analyze.sh
@@ -8,7 +8,7 @@ mkdir -p ${LOG_DIR}
 if [ -f ${LOG_DIR}/alp.log ]; then
     sudo mv ${LOG_DIR}/alp.log ${LOG_DIR}/alp.log.$(date "+%Y%m%d_%H%M%S")
 fi
-sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r > ${LOG_DIR}/alp.log
+sudo /usr/local/bin/alp -f /var/log/nginx/access.log --sum -r  --aggregates='/isu/.*/graph,/api/condition/.*,/api/isu/.*/icon,/isu/.*/condition,/api/isu/[a-f0-9\-],/isu/[a-f0-9\-]' > ${LOG_DIR}/alp.log
 
 # slow query
 if [ -f ${LOG_DIR}/mysql-slow-query.log ]; then
